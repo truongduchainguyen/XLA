f752237771f596c6529a3df3fe7a65cf862f8d76
Duong Tien <4401104193@student.hcmue.edu.vn>
2020-10-24 23:17:52 +0700

    renamed dialog and add some feature to .ui(s)

---
+++ masterUI/qdialog_brightness_contrast.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+
+class Ui_dialog_brightness_contrast(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('ui/dialog_brightness_contrast.ui', self)
+        '''preloaded'''
+
+        '''find children'''
+        self.lbl_brightness_value: QtWidgets.QLabel = self.findChild(QtWidgets.QLabel, 'lbl_brightness_value')
+        self.lbl_contrast_value: QtWidgets.QLabel = self.findChild(QtWidgets.QLabel, 'lbl_contrast_value')
+        self.hslider_brightness: QtWidgets.QSlider = self.findChild(QtWidgets.QSlider, 'hslider_brightness')
+        self.hslider_contrast: QtWidgets.QSlider = self.findChild(QtWidgets.QSlider, 'hslider_contrast')
+        self.btnbox: QtWidgets.QDialogButtonBox = self.findChild(QtWidgets.QDialogButtonBox, 'btnbox')
+
+        self.hslider_contrast.valueChanged['int'].connect(lambda: self.lbl_contrast_value.setNum(self.hslider_contrast.value()/10))
+
+
+
+        self.show()
